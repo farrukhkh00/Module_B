efa6a3fe661daa33dde6af0d4651eecdcc896977
farrukhkh00 <155253455+farrukhkh00@users.noreply.github.com>
2024-07-02 22:52:11 +0500

    assignment 5

---
+++ Assignment 5/react_proj/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1>My First React Project</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, assumenda.</p>
+      <img src="./public/vite.svg" alt="" />
+    </>
+  )
+}
+
+export default App
