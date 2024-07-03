87d4234c7a099ab45a6bc8c17ec4b88f2447c2d2
farrukhkh00 <155253455+farrukhkh00@users.noreply.github.com>
2024-07-03 17:52:10 +0500

    assingment 6

---
+++ as 6/as 6/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav_one from './Nav_one/Nav_one'
+import Nav_two from './Nav_two/Nav_two'
+
+const App = () => {
+  return (
+    <>
+      <Nav_one/>
+      <Nav_two/>
+    </>
+  )
+}
+
+export default App
